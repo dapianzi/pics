67727a70ab7c1017078a866315f5317d505f6abd
Carl <ee@aa.com>
2017-12-27 18:36:05 +0800

    class view,templateview template extend

--- cats/urls.py
+++ cats/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='home'),
+    url(r'^$', views.IndexView.as_view(), name='home'),
     url(r'^signin/', views.signin, name='signin'),
     url(r'^signout/', views.signout, name='signout'),
     url(r'^likes/$', views.likes, name='likes'),
--- cats/views.py
+++ cats/views.py
@@ -1,21 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .controllers import indexController
+from django.views.generic.base import View,TemplateView,RedirectView
+from django.shortcuts import render,redirect,get_object_or_404,get_list_or_404
+from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+import cats.models as Cat_models
 # Create your views here.
 
-def index(request):
-    #return render(request, 'cats/index.html')
-    return indexController.index(request)
+class IndexView(TemplateView):
+    template_name = 'cats/index.html'
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['imgs'] = Cat_models.CatImgs.objects.all()[:15]
+        return context
 
 def signin(request):
-    return indexController.signin(request)
+    return render(request, 'cats/signin.html')
 
 def signout(request):
-    return indexController.signout(request)
+    logout(request)
 
-def likes(request):
-    pass
-def delete(request):
-    pass
-def more(request):
-    pass
+class LikesView(View):
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Hello, World!')
+@login_required
+def DelView(RedirectView):
+    def post(self, request, id, *args, **kwargs):
+        ImgModel = Cat_models.CatImgs
+        img = get_list_or_404(ImgModel, id=id)
+
+
+class MoreView(TemplateView):
+    def get_context_data(self, **kwargs):
+        context = super(MoreView, self).get_context_data(**kwargs)
+        p = int(kwargs['p'])
+        context['list'] = Cat_models.CatImgs.all()[(p-1)*5:5]
+        return context
