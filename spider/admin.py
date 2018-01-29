2c3ffc9314a5d91d1600334b8a5f7d63684eeb86
Carl <ee@aa.com>
2018-01-29 18:53:37 +0800

    conblogs scraping 01.26 19:04

--- spider/admin.py
+++ spider/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from spider.models import Spider, ContentType, SpiderTask
 # Register your models here.
+
+admin.site.register(ContentType)
+admin.site.register(Spider)
+admin.site.register(SpiderTask)
