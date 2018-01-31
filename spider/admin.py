10e13576e369c461fb9c561a2d31d7311bfbb155
Carl <ee@aa.com>
2018-01-31 17:26:56 +0800

    admin 添加批量操作actions 下载csv测试

--- spider/admin.py
+++ spider/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
+from django.http import HttpResponse, StreamingHttpResponse
+from django.core import serializers
 from spider.models import Spider, ContentType, SpiderTask, BlackList
 # Register your models here.
 
-admin.site.register(ContentType)
+@admin.register(ContentType)
+class ContentTypeAdmin(admin.ModelAdmin):
+    # 列表项
+    list_display = ['name', 'max_item', 'expire_time', 'active']
+    # 批量操作
+    actions = [
+        'set_active',
+        'set_discard',
+        'export_csv',
+    ]
+
+    def set_active(self, request, queryset):
+        r = queryset.update(active=0)
+        self.message_user(request, "%s个类别被激活" % r)
+    # 设置操作描述
+    set_active.short_description = '激活'
+    def set_discard(self, req, qys):
+        r = qys.update(active=1)
+        self.message_user(req, "%s个类别被激活" % r)
+    set_discard.short_description = '禁用'
+    def export_csv(self, request, queryset):
+        """导出csv"""
+        response = StreamingHttpResponse('\n'.join([','.join(
+            # list元素转str
+            list(map(lambda x: str(x), x))
+        ) for x in list(
+            # queryset 转 list
+            queryset.values_list('name', 'max_item', 'expire_time', 'active')
+        )]).encode('gbk').decode('gbk'), charset='gbk', content_type='attachment/csv')
+        response['Content-Disposition'] = 'attachment;filename="test.csv"'
+        return response
+    export_csv.short_description = '导出csv'
 
 @admin.register(Spider)
 class SpiderAdmin(admin.ModelAdmin):
